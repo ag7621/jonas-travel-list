36d7b6756b9b840bc0cfe0c5ea9d5e16742cf713
ag7621 <aaron.c.guerra@gmail.com>
2025-01-30 13:08:30 -0700

    refactored components
    moved components to separate files, added clear list function

---
+++ src/Components/Item.jsx
@@ -0,0 +1,19 @@
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li id={item.id}>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
+  );
+}
+
+export default Item;
